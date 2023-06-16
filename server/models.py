078111aebce2fad7ab3400995502760b4a22dcba
Freddie Mazzilli <freddie.mazzilli@gmail.com>
2023-06-16 12:17:47 -0400

    Completes validations flask sqlalchemy validations lab

--- server/models.py
+++ server/models.py
@@ -12,6 +12,18 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_name(self, key, name):
+        if name == '':
+            raise ValueError('Author must have a name')
+        return name
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("Phone number must be 10 digits.")
+        return phone_number
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -26,6 +38,31 @@ class Post(db.Model):
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) <= 250:
+            raise ValueError('Post content must be at least 250 characters.')
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) >= 250:
+            raise ValueError('Summary can be a maximum of 250 characters.')
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if not category in ['Fiction', 'Non-Fiction']:
+            raise ValueError('Category must be Fiction or Non-Fiction.')
+        return category
+
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not title in clickbait:
+            raise ValueError('Post title must be clickbait-y.')
+        return title
 
 
     def __repr__(self):
